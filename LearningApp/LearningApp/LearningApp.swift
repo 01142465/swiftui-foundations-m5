f8b50c4558901c9ca1d95b97750399e90f72dbdf
david.estrada <01142465d@gmail.com>
2021-07-31 12:34:15 -0500

    -create environment object

---
+++ LearningApp/LearningApp/LearningApp.swift
@@ -0,0 +1,18 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by David Estrada on 7/31/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+                .environmentObject(contentModel())
+        }
+    }
+}
---
+++ LearningApp/LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by David Estrada on 7/31/21.
+//
+
+import Foundation
+
+class contentModel: ObservableObject {
+    
+}
